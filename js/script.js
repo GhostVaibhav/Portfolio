3d3522175c5e206a2c385d727ba794b79b767f19
GhostVaibhav <48472541+GhostVaibhav@users.noreply.github.com>
2021-06-15 11:26:23 +0530

    Updated the structure of program

--- js/script.js
+++ js/script.js
@@ -1,31 +1,3 @@
-// function changeColor(color) {
-//     if(msg != "") {
-//         document.getElementById("container").style.backgroundColor = color;
-//         document.getElementById("input-contain").style.backgroundColor = color;
-//         document.getElementById("name").style.backgroundColor = color;
-//     }
-// }
-// function formSubmit() {
-//     msg = document.getElementById("input-contain").value;
-//     if(msg != "") {
-//         document.getElementById("input-contain").style.display = 'none';
-//         document.getElementById("lets-go").style.opacity = 0;
-//         document.getElementById("name").innerHTML = msg;
-//         document.getElementById("name").style.display = 'flex';
-//     }
-// }
-// function Enter() {
-//     if(event.keyCode == 13) {
-//         msg = document.getElementById("input-contain").value;
-//         if(msg != "") {
-//             document.getElementById("input-contain").style.display = 'none';
-//             document.getElementById("lets-go").style.opacity = 0;
-//             document.getElementById("name").innerHTML = msg;
-//             document.getElementById("name").style.display = 'flex';
-//         }
-//     }
-// }
-
 window.mobileCheck = function () {
     let check = false;
     (function (a) {
